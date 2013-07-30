301276255b0b7195a473ca36de5c4a5a97b9e743
= <=>
2013-07-29 23:59:06 -0400

    Finished flow control

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,6 +11,19 @@ define(function() {
       //
       // otherwise the function should return the number, or false if no number
       // was provided or the value provided is not a number
+      var ret = null;
+
+      if (num % 15 === 0) {
+        ret = 'fizzbuzz';
+      } else if (num % 5 === 0) {
+        ret = 'buzz';
+      } else if (num % 3 === 0) {
+        ret = 'fizz';
+      }
+
+      // If the input of parseInt is not a number it will return NaN
+      // so this trick works.
+      return ret || parseInt(num) || false;
     }
   };
 });
