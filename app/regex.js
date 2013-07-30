f69df127aabd85d11108211ee6ef08008dae3f56
= <=>
2013-07-30 00:56:39 -0400

    Finished regular expressions

--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,41 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        return (/\d/).test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+        // Since this is a *JavaScript* assessment and not a RegEx one.
+        // I shamelessly cheated looking for the corrent expression for this one.
+        // http://stackoverflow.com/questions/178837/how-can-i-find-repeated-letters-with-a-perl-regex
+        return (/([a-zA-Z])\1+/).test(str);
     },
 
     endsWithVowel : function(str) {
-
+        return (/[aAeEiIoOuU]$/).test(str);
     },
 
     captureThreeNumbers : function(str) {
+        var ret = false;
+        var regex = (/\d{3}/);
 
+        if (regex.test(str)) {
+            ret = regex.exec(str)[0];
+        }
+
+        return ret;
     },
 
     matchesPattern : function(str) {
+        var pattern = (/^\d{3}-\d{3}-\d{4}$/);
 
+        return pattern.test(str);
     },
     isUSD : function(str) {
+        // This can probably be made better.
+        var usdPattern = /^(\$\d{1,3},?)(\d{3},?)*(\.\d{2})?$/
 
+        return usdPattern.test(str);
     }
   };
 });
